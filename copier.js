96722e8300fad65c3a34b9c5ddf4c9f81fae1671
Alistair Christie <hubwriter@github.com>
2020-10-05 19:41:05 +0100

    Add selection of copied text

--- copier.js
+++ copier.js
@@ -21,14 +21,50 @@ function copyMain() {
             });
         */
         
+        var startSelection, endSelection, moveLeftBy, moveRightBy;
+        startSelection = endSelection = editor.document.offsetAt(editor.selection.anchor);
+        // Get the position of the start of the reusable
+        // Do this by parsing each character from the cursor leftwards
+        // until reaching "{"
+        for (let parseCharacter = ""; parseCharacter != "{"; startSelection--) {
+            let startPosition = editor.document.positionAt(startSelection);
+            let stopPosition = editor.document.positionAt(startSelection+1);
+            let textRange = new vscode.Range(startPosition, stopPosition);
+            parseCharacter = editor.document.getText(textRange);
+            // console.log('startSelection = ' + startSelection + '; parseCharacter = ' + parseCharacter); 
+            moveLeftBy =  endSelection - startSelection;
+            // console.log('move left by = ' + moveLeftBy);  
+        }
+        // Get the position of the end of the reusable by parsing forwards
+        for (let parseCharacter = ""; parseCharacter != "}"; endSelection++) {
+            let startPosition = editor.document.positionAt(endSelection);
+            let stopPosition = editor.document.positionAt(endSelection+1);
+            let textRange = new vscode.Range(startPosition, stopPosition);
+            parseCharacter = editor.document.getText(textRange);
+            // console.log('endSelection = ' + endSelection + '; parseCharacter = ' + parseCharacter);
+            moveRightBy =  endSelection - startSelection;
+            // console.log('move left by = ' + moveRightBy);      
+        }       
+        
+        // Move the cursor to the start of the reusable
+        vscode.commands.executeCommand("cursorMove", {
+            to: "left",
+            by: "character",
+            value: moveLeftBy
+        })  
+        // Move the cursor to the end of the reusable, selecting the text moved over
+        vscode.commands.executeCommand("cursorMove", {
+            to: "right",
+            by: "character",
+            value: moveRightBy,
+            select: true
+        })              
+
     }
     else {
         vscode.window.showInformationMessage("Open reusales extension: cursor is not within a reusable or variable");
     }
 
-
-    
-    
 }
 
 module.exports = {
