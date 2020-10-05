74457778b1214831dc271e2091d10ec111147688
Alistair Christie <hubwriter@github.com>
2020-10-05 10:28:43 +0100

    Copy the reusable at the cursor point - WiP

--- copier.js
+++ copier.js
@@ -9,8 +9,18 @@ function copyMain() {
     var reusableString = shared.getReusableString(editor);
 
     if (reusableString != "") {
-        vscode.window.showInformationMessage('reusableString = ' + reusableString);
-        console.log('reusableString = ' + reusableString);
+        // console.log('reusableString = ' + reusableString);
+
+        // Write to clipboard
+        vscode.env.clipboard.writeText(reusableString);
+        /* 
+            // Check what's in the clipboard now:
+            vscode.env.clipboard.readText().then((text)=>{
+                let clipboard_content = text; 
+                console.log('clipboard_content = ' + clipboard_content);
+            });
+        */
+        
     }
     else {
         vscode.window.showInformationMessage("Open reusales extension: cursor is not within a reusable or variable");
